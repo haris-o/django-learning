a06cb5ec304816af75ad0553855b28dd08694736
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-05-30 18:19:11 +0200

    fixed settings regarding user auth

--- recordcompany/recordcompany/settings.py
+++ recordcompany/recordcompany/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
