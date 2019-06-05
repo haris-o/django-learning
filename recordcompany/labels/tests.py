712d18bdca088c716fc3c23c6f6c6cc4f2904579
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-06-05 09:55:01 +0200

    added first test

--- recordcompany/labels/tests.py
+++ recordcompany/labels/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+from .views import LabelViewSet
+
+
+class TestLabel(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = LabelViewSet.as_view({'get': 'list'})
+        self.uri = '/labels/'
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+        response = self.view(request)
+        status_code = response.status_code
+        self.assertEqual(status_code, 200, 'Expected status 200, got {}'.format(status_code))
