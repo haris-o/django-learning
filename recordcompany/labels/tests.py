e5f3b6fd9c4035a4aa5f526b20f12dddedb4c911
haris-osmanbegovic <haris.osmanbegovic1@gmail.com>
2019-06-05 10:04:01 +0200

    added user creation and authorization to test

--- recordcompany/labels/tests.py
+++ recordcompany/labels/tests.py
@@ -1,4 +1,6 @@
 from rest_framework.test import APITestCase, APIRequestFactory
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 from .views import LabelViewSet
 
@@ -9,8 +11,25 @@ class TestLabel(APITestCase):
         self.view = LabelViewSet.as_view({'get': 'list'})
         self.uri = '/labels/'
 
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='test@test.com',
+            password='test'
+        )
+
     def test_list(self):
-        request = self.factory.get(self.uri)
+        request = self.factory.get(
+            self.uri,
+            HTTP_AUTHORIZATION='Token {}'.format(self.token.key)
+        )
+        request.user = self.user
         response = self.view(request)
         status_code = response.status_code
         self.assertEqual(status_code, 200, 'Expected status 200, got {}'.format(status_code))
